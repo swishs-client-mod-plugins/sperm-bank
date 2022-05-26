a741ced517ab0723baa16cffb64d8938aa5d82cd
Swishilicous <paige.git@gmail.com>
2022-05-26 15:39:54 -0500

    fix latest headerbar update

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 /* The below code is licensed under MIT. */
 
-import { after, findAndPatch as lazy } from '@cumcord/patcher';
+import { after, before, findAndPatch as lazy } from '@cumcord/patcher';
 import { findByDisplayName, findByProps } from '@cumcord/modules/webpack';
 
 import Logger from './src/modules/Logger';
@@ -9,7 +9,6 @@ import Receptionist from './src/modules/Receptionist';
 import { ContextMenu } from './src/modules/Modules';
 import { pjoin, join, callArgs, findInReactTree } from './src/modules/Utilities';
 
-// @ts-ignore
 import injectStyles from './src/style.scss';
 import BankIcon from './src/components/icons/Bank';
 import SpermBank from './src/components/modals/SpermBank';
@@ -20,18 +19,26 @@ const { openModal } = findByProps('openModal', 'openModalLazy');
 const headerClasses = findByProps('icon', 'isHeader');
 const iconClasses = findByProps('iconWrapper', 'clickable');
 const MiniPopover = findByDisplayName('MiniPopover', false);
-const HeaderBarContainer = findByDisplayName('HeaderBarContainer');
+const HeaderBarContainer = findByDisplayName('HeaderBarContainer', false);
 
 let patches: Function[] = [];
+patches.push(before('push', patches, ([patch]: [Function]) => {
+  try {
+    return patch();
+  } catch (err) {
+    Logger.error('Failed to patch', err, patch);
+  }
+}));
+
 const patchHeaderBarContainer = () => {
-  return after('render', HeaderBarContainer.prototype, (_, ret) => {
-    const Header = findInReactTree(ret, c => Array.isArray(c.toolbar?.props?.children));
-    if (!Header) return Logger.warn('Could not patch HeaderBar icons!');
+  return after('default', HeaderBarContainer, (_, ret) => {
+    const Header = findInReactTree(ret, n => Array.isArray(n.toolbar));
+    if (!Header) return Logger.warn('Could not patch the HeaderBar!');
 
     // check in case things fail to unpatch for whatever reason
-    if (findInReactTree(Header.toolbar, c => c?.props?.id === 'sperm')) return;
+    if (findInReactTree(Header.toolbar, n => n?.props?.id === 'sperm')) return;
 
-    Header.toolbar.props.children.splice(3, 0,
+    Header.toolbar.splice(3, 0,
       <TooltipContainer
         text='Sperm Bank' position='bottom' id='sperm'
         className={join(iconClasses.iconWrapper, iconClasses.clickable)} >
@@ -99,10 +106,10 @@ const patchMiniPopover = () => {
 export default () => {
   return {
     onLoad() {
-      patches.push(injectStyles());
-      patches.push(patchMiniPopover());
-      patches.push(patchHeaderBarContainer());
-      patches.push(patchMessageContextMenu());
+      patches.push(injectStyles);
+      patches.push(patchMiniPopover);
+      patches.push(patchHeaderBarContainer);
+      patches.push(patchMessageContextMenu);
 
       // for my debugging purposes; UAYOR
       (window as any).Receptionist = Object.assign({ version: '1.0.0' }, Receptionist);
